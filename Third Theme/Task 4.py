79b6a09507cdafaf5fdc37a19f8f13bd54cd46df
EridanShen <eridan1310@gmail.com>
2024-06-23 17:45:46 +0700

    Третья тема

--- Third Theme/Task 4.py
+++ Third Theme/Task 4.py
@@ -1,9 +1,9 @@
 # Обход списка
-my_list = [1, 'hello', 3.14, True]
+my_list = [1, 'Hello', 3.14, True]
 for item in my_list:
     print(item)
 
 # Обход словаря
-my_dict = {'name': 'Alice', 'age': 25, 'city': 'New York'}
+my_dict = {'name': 'Aiden', 'age': 25, 'city': 'New York'}
 for key, value in my_dict.items():
     print(f"{key}: {value}")
--- Third Theme/Task 6.py
+++ Third Theme/Task 6.py
@@ -1,2 +1,2 @@
 total = sum(range(1, 101))
-print("Сумма всех чисел от 1 до 100:", total)
+print("Сумма всех чисел:", total)
